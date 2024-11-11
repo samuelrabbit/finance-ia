6336f245b8cc09acf4c006775f478fa7d566c827
samuelrabbit <samuel4050@hotmail.com>
2024-11-11 10:20:15 -0300

    fix: add components in list eslint

--- app/_components/ui/chart.tsx
+++ app/_components/ui/chart.tsx
@@ -69,7 +69,8 @@ ChartContainer.displayName = "Chart";
 
 const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
   const colorConfig = Object.entries(config).filter(
-    ([, config]) => config.theme || config.color,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    ([_, config]) => config.theme || config.color,
   );
 
   if (!colorConfig.length) {
